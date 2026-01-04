00e799a308615375c85475e0e03104d36e501b0b
Loup-Garou911XD <90267658+Loup-Garou911XD@users.noreply.github.com>
2026-01-05 02:28:55 +0530

    asg

---
+++ plugins/utilities/test_plugin.py
@@ -0,0 +1,10 @@
+plugman = dict(
+    plugin_name="test_plugin",
+    description="A test plugin for demonstration purposes blah blah.",
+    external_url="https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+    authors=[
+        {"name": "Loup", "email": "loupg450@gmail.com", "discord": "loupgarou_"},
+        {"name": "brostos", "email": "", "discord": "brostos"}
+    ],
+    version="1.0.0",
+)
