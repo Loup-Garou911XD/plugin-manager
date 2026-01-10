e5a581bf45a90ae99abef5b338c814b41b827539
Loup-Garou911XD <90267658+Loup-Garou911XD@users.noreply.github.com>
2026-01-10 15:52:15 +0530

    test plugins

---
+++ plugins/utilities/test_plugin.py
@@ -0,0 +1,10 @@
+# ba_meta require api 9
+plugman = dict(
+    plugin_name="test_plugin",
+    description="googoo gaagaa",
+    external_url="",
+    authors=[
+        {"name": "loupy", "email": "", "discord": ""},
+    ],
+    version="1.0.0",
+)
